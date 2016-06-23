9107e2a3a57bf36036b30ab4541d1c75fa5c5ad9
Samuel Amoah <snamoah@gmail.com>
2016-06-23 13:44:39 +0000

    remove insecure and use methods for handling collection operations

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -21,12 +21,7 @@ export default class App extends Component {
 
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text: text, 
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', text);
 
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
